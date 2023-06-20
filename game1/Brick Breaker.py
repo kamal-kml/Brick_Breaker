e4f6c8c865d3396eba309e8d79a5bf9247273906
Kamal <kamaldn3@gmail.com>
2023-06-20 10:32:33 +0530

    Canvas created

--- game1/Brick Breaker.py
+++ game1/Brick Breaker.py
@@ -5,5 +5,10 @@ window.geometry("1000x600")
 window.resizable(0,0)
 window.title("BRICK BREAKER")
 
+#Canvas
+
+canvas = Canvas(bg='black',width=1000,height=600)
+canvas.pack()
+
 
 window.mainloop()
